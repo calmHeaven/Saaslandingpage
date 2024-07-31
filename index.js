b7677e6862dc05f7e23290b3a6fe82c4236ca9e0
ABENEZER TESSEMA <abenezer4989@gmail.com>
2024-07-31 10:31:24 -0500

    finalized

--- index.js
+++ index.js
@@ -1,28 +1,39 @@
-document.getElementById('waitlist-form').addEventListener('click', function() {
-    
-    event.preventDefault();
-    fetch('http://localhost:8080/api/Waitlist', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(jsonObject)
-    })
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-        return response.json(); // Parse response body as JSON
-    })
-    .then(data => {
-        console.log('Waitlist registration successful', data);
-        alert('Thank you for signing up! We will notify you when we launch. 👊🏿');
-        // Redirect to another page, show success message, or perform other actions
-    })
-    .catch(error => {
-        console.error('Error registering:', error);
-        alert('Error registering: ' + error.message);
-        // Handle registration errors or display error messages to the user
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('waitlistForm').addEventListener('submit', function (event) {
+        document.getElementById('waitlistForm').addEventListener('submit', function () {
+        let submission = docuemtn.getElementById('');
+        });
+            //event.preventDefault(); // Prevent the default form submission
+
+            var formData = new FormData(this); // Get form data
+            var jsonObject = {}; // Convert form data to JSON object
+            formData.forEach(function (value, key) {
+                jsonObject[key] = value;
+            });
+
+
+            // Send form data to backend API
+            fetch('http://localhost:8080/api/v1/Waitlist', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(jsonObject)
+            })
+                .then(response => {
+                    if (!response.ok) {
+                        throw new Error('Network response was not ok');
+                    }
+                    return response.json(); // Parse response body as JSON
+                })
+                .then(data => {
+                    console.log('Waitlist entry successful:', data);
+                    alert('Successfully joined the waitlist!');
+                })
+                .catch(error => {
+                    console.error('Error joining waitlist:', error);
+                    alert('Error joining waitlist: ' + error.message);
+                });
+        });
     });
 });
-
